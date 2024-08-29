3f085aa1f129d37d25578daec9746ca6ba64fd98
Fern-Aerell <fernaerell2020@gmail.com>
2024-08-30 00:14:46 +0700

    Membuat data akun admin di database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,23 +17,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'habib',
-            'email' => 'habib@admin.com',
-        ]);
-
-        User::factory()->create([
-            'name' => 'nia',
-            'email' => 'nia@admin.com',
-        ]);
-
-        User::factory()->create([
-            'name' => 'aerel',
-            'email' => 'aerel@admin.com',
-        ]);
-
-        User::factory()->create([
-            'name' => 'nico',
-            'email' => 'nico@admin.com',
+            'name' => 'Admin',
+            'username' => 'admin',
+            'email' => 'admin@kitadiskusi.com',
+            'password' => Hash::make('admin#124'),
         ]);
     }
 }
