f1fb679e3e3a8093eb988a9cc2b467d64b9b49dd
kurniaaa01 <husnulkurnia299@gmail.com>
2024-08-29 14:56:22 +0700

    membuat migration dan design erd

--- database/migrations/2024_08_29_050500_create_topics_table.php
+++ database/migrations/2024_08_29_050500_create_topics_table.php
@@ -11,19 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('topics', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->foreignId('user_id')->constrained();
+            $table->string('title');
+            $table->text('body');
+            $table->string('category');
             $table->timestamp('created_at');
-            $table->timestamp("updated_at");
-            
-        });
+            $table->timestamp('updated_at');
 
-       
+        });
     }
 
     /**
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('topics');
     }
 };
--- database/migrations/2024_08_29_071617_create_votes_table.php
+++ database/migrations/2024_08_29_071617_create_votes_table.php
@@ -11,19 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('votes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->foreignId('coment_id');
+            $table->foreignId('user_id');
+            $table->string('vote_type');
             $table->timestamp('created_at');
-            $table->timestamp("updated_at");
-            
         });
-
-       
     }
 
     /**
@@ -31,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('votes');
     }
 };
