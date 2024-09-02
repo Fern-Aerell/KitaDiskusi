6ba1b8ba06c747b33604be8ee4d4fd1c9b7cd380
HabibAsyrof <habibasyrof2006@icloud.com>
2024-09-02 13:43:59 +0700

    Memperbarui view dan menyesuaikan dengan auth controller dan route yang baru

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -12,10 +12,25 @@
 <body>
     <div class="halaman-login">
         <div class="container">
-            <form action="/login" method="post">
+            <form action="{{ route('signup.store') }}" method="post">
+                @csrf
                 <h1>Sign Up</h1>
+                <br/>
+                <div class="name">
+                    <input type="text" name="name" id="name" placeholder="Name">
+                    @error('name')
+                        <p>{{ $message }}</p>
+                    @enderror
+                </div>
+                <br/>
+                <div class="username">
+                    <input type="text" name="username" id="username" placeholder="Username">
+                    @error('username')
+                        <p>{{ $message }}</p>
+                    @enderror
+                </div>
+                <br/>
                 <div class="email">
-                    @csrf
                     <input type="text" name="email" id="email" placeholder="Email">
                     @error('email')
                         <p>{{ $message }}</p>
@@ -28,10 +43,10 @@
                         <p>{{ $message }}</p>
                     @enderror
                 </div>
-                <div class="confirm-password">
+                <div class="password_confirmation">
                     <br>
-                    <input type="password" name="confirm-password" id="confirm-password" placeholder="Confirm Password">
-                    @error('confirm-password')
+                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password">
+                    @error('password_confirmation')
                         <p>{{ $message }}</p>
                     @enderror
                 </div>
@@ -42,7 +57,7 @@
         </div>
         <div class="regis">
             <p>sudah punya akun? </p>
-            <a href="/login"><p>Sign In</p></a>
+            <a href="{{ route('login') }}"><p>Sign In</p></a>
         </div>
     </div>
 </body>
