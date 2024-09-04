ab81882cac694b618bc89297bb39644cb6369c9c
HabibAsyrof <habibasyrof2006@icloud.com>
2024-09-04 14:02:10 +0700

    revisi login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,32 +8,32 @@
 
 @section('content')
     <div class="halaman-login">
-        <div class="container">
-            <form action="{{ route('login.auth') }}" method="post">
-                <h1>Sign In</h1>
-                <p>{{ session('success') }}</p>
-                <div class="email">
-                    @csrf
-                    <input type="text" name="email" id="email" placeholder="Email">
-                    @error('email')
-                        <p>{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="password">
-                    <br>
-                    <input type="password" name="password" id="password" placeholder="Password">
-                    @error('password')
-                        <p>{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="button">
-                    <button type="submit">Masuk</button>
-                </div>
-            </form>
-        </div>
-        <div class="regis">
-            <p>belum punya akun? </p>
-            <a href="{{ route('signup') }}"><p>Sign Up</p></a>
-        </div>
+        <form action="{{ route('login.auth') }}" method="post">
+            <img src="kitadiskusi_logo.png" alt="" style="background-color: black">
+            <p>{{ session('success') }}</p>
+            <div class="email">
+                @csrf
+                <input type="text" name="email" id="email" placeholder="Email">
+                @error('email')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="password">
+                <br>
+                <input type="password" name="password" id="password" placeholder="Password">
+                @error('password')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="regis">
+                <p>belum punya akun? </p>
+                <a href="{{ route('signup') }}">
+                    <p>Sign Up</p>
+                </a>
+            </div>
+            <div class="button">
+                <button type="submit">Masuk</button>
+            </div>
+        </form>
     </div>
 @endsection
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -8,53 +8,54 @@
 
 @section('content')
     <div class="halaman-login">
-        <div class="container">
-            <form action="{{ route('signup.store') }}" method="post">
-                @csrf
-                <h1>Sign Up</h1>
-                <br/>
-                <div class="name">
-                    <input type="text" name="name" id="name" placeholder="Name">
-                    @error('name')
-                        <p>{{ $message }}</p>
-                    @enderror
-                </div>
-                <br/>
-                <div class="username">
-                    <input type="text" name="username" id="username" placeholder="Username">
-                    @error('username')
-                        <p>{{ $message }}</p>
-                    @enderror
-                </div>
-                <br/>
-                <div class="email">
-                    <input type="text" name="email" id="email" placeholder="Email">
-                    @error('email')
-                        <p>{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="password">
-                    <br>
-                    <input type="password" name="password" id="password" placeholder="Password">
-                    @error('password')
-                        <p>{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="password_confirmation">
-                    <br>
-                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password">
-                    @error('password_confirmation')
-                        <p>{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="button">
-                    <button type="submit">Daftar</button>
-                </div>
-            </form>
-        </div>
-        <div class="regis">
-            <p>sudah punya akun? </p>
-            <a href="{{ route('login') }}"><p>Sign In</p></a>
-        </div>
+        <form action="{{ route('signup.store') }}" method="post">
+            @csrf
+            <img src="kitadiskusi_logo.png" alt="" style="background-color: black">
+            <br />
+            <div class="name">
+                <input type="text" name="name" id="name" placeholder="Name">
+                @error('name')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
+            <br />
+            <div class="username">
+                <input type="text" name="username" id="username" placeholder="Username">
+                @error('username')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
+            <br />
+            <div class="email">
+                <input type="text" name="email" id="email" placeholder="Email">
+                @error('email')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="password">
+                <br>
+                <input type="password" name="password" id="password" placeholder="Password">
+                @error('password')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="password_confirmation">
+                <br>
+                <input type="password" name="password_confirmation" id="password_confirmation"
+                    placeholder="Confirm Password">
+                @error('password_confirmation')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="regis">
+                <p>sudah punya akun? </p>
+                <a href="{{ route('login') }}">
+                    <p>Sign In</p>
+                </a>
+            </div>
+            <div class="button">
+                <button type="submit">Daftar</button>
+            </div>
+        </form>
     </div>
 @endsection
