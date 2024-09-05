5e59de8825b26f97abf2871060e657d64b3c5279
Fern-Aerell <fernaerell2020@gmail.com>
2024-09-06 02:19:55 +0700

    Menambahkan credits

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="head-home text-center text-lg-start mt-5">
     <div class="text-center p-3">
-        © 2024 Forum Diskusi
+        © 2024 KitaDiskusi - Kelompok 2 - Aerell, Nico, Habib, Nia
     </div>
 </footer>
